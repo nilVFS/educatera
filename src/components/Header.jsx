e2f2693a594b57a65665b1da15564938bdf0a57a
Владислав Низьев <v.nizjev2013@yandex.ru>
2023-03-17 20:05:47 +0300

    fill navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
         <header>
             <h3>EDUCATERA</h3>
             <nav>
-
+                <ul>
+                    <li>Главная</li>
+                    <li>Группы</li>
+                    <li>Профиль</li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </nav>
         </header>
     )
