f9504bd39bbd519913fa6def2627cbaea1b21630
Владислав Низьев <v.nizjev2013@yandex.ru>
2023-03-17 19:55:07 +0300

    create header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 export default function Header() {
     return (
         <header>
+            <h3>EDUCATERA</h3>
             <nav>
-                
+
             </nav>
         </header>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import Header from "../components/Header";
+
 export default function Home() {
     return (
-        <>
+        <main>
+            <Header />
             <h1>Homepage</h1>
-        </>
+        </main>
     )
 }
